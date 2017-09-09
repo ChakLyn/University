91d5424cfa89a0fa17e1af7f262c91fe98ebac85
Drew <znodemi@gmail.com>
2017-09-09 21:11:38 +0300

    [AI Dialog]
    ChangeLog: Have added Phrases for dialog

---
+++ AI/1Lab/AILab_1/AILab_1/AI/Core/DialogCore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AILab_1.AI.Core
+{
+    public class DialogCore
+    {
+        public DialogCore()
+        {
+                
+        }
+    }
+}
---
+++ AI/1Lab/AILab_1/AILab_1/Utils/JsonHelper/JsonSerializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AILab_1.Utils.JsonHelper
+{
+    class JsonSerializer
+    {
+    }
+}
