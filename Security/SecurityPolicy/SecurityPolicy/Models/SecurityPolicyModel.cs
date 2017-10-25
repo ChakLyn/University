2d0229bd2196b84991d0e61244a6e7d1f4a21513
Drew <znodemi@gmail.com>
2017-10-25 22:01:40 +0300

    [Security_SecurityPolicy]
    ChnageLog: Have fully realized security policy modelling core.

---
+++ Security/SecurityPolicy/SecurityPolicy/Models/SecurityPolicyModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecurityPolicy.Models
+{
+    public class SecurityPolicyModel
+    {
+        public List<UserModel> Users { get; set; }
+        public List<string> Rights { get; set; }
+        public List<string> Objects { get; set; }
+    }
+}
---
+++ Security/SecurityPolicy/SecurityPolicy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SecurityPolicy.Utils.JsonHelper;
+using SecurityPolicy.Models;
+using SecurityPolicy.Core;
+
+namespace SecurityPolicy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SecurityPolicyModel securityPolicyModel = JsonSerializer.DeserializeSecurityPolicyModel(
+                JsonSerializer.GetJson("Resources\\SecurityPolicyConfig.json"));
+
+            SecurityPolicyImitation core = new SecurityPolicyImitation(securityPolicyModel);
+            core.StartImitationSystem();
+        }
+    }
+}
