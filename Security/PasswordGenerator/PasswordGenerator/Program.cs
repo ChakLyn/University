0a1b43d7cef262d3954def9162176be841e29aeb
Drew <znodemi@gmail.com>
2017-10-26 09:14:45 +0300

    [Security_PasswordGenerator]
    ChangeLog: Have realized password generator (wich take strength, lenght and count of passwords).

---
+++ Security/PasswordGenerator/PasswordGenerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PasswordGenerator.Utils;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter strength for password: ");
+            int strength = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter length for password: ");
+            int length = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter count of passwords: ");
+            int count = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(Generator.GeneratePassword(strength,length,count));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Security/PasswordGenerator/PasswordGenerator/Utils/Generator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator.Utils
+{
+    public static class Generator
+    {
+        private static List<char> _range = new List<char>();
+
+        private static Random random = new Random();
+
+        static Generator()
+        {
+            _range.AddRange(Enumerable.Range('A', 26).Select(item => (char)item));
+            _range.AddRange(Enumerable.Range('a', 26).Select(item => (char)item));
+            _range.AddRange(Enumerable.Range('0', 10).Select(item => (char)item));
+            _range.AddRange(Enumerable.Range('!', 7).Select(item => (char)item));
+            _range.AddRange(Enumerable.Range('А', 32).Select(item => (char)item));
+            _range.AddRange(Enumerable.Range('а', 32).Select(item => (char)item));
+        }
+
+        public static string GeneratePassword(int strength, int length, int count)
+        {
+            StringBuilder password = new StringBuilder();
+
+            for (int j = 0; j < count; j++)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    password.Append(_range[random.Next(0, strength)]);
+                }
+                password.AppendLine();
+            }
+            return password.ToString();
+        }
+    }
+}
